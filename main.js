ab98f948357aff79388b0983fed9eb6b3f273993
amber officil <amberofficial380@gmail.com>
2024-04-25 01:20:13 +0500

    function assignment

---
+++ main.js
@@ -0,0 +1,64 @@
+"use strict";
+//function mySum(){
+//console.log(10+20)
+//}
+//mySum();
+//function mySum2(){
+// let a = 20;
+//let b = 20;
+//let sum = a+b;
+// console.log(sum)
+//}
+//mySum2();
+//  Argument And Parameters
+//function print(greeting: string){
+//console.log(greeting + "  Ali")
+//}
+//print('hello');
+//function mySum(a: number,b:number){
+// let sum = a + b;
+//console.log(sum);
+//}
+//mySum(20,30);
+//function mySum(){
+//let c = 5;
+//let d = 10;
+//let sum = c + d;
+//console.log(sum);
+//return sum;
+//}
+//mySum();
+//console.log(mySum());
+//function greeting(str: string){
+//return str + '  ali'
+//}
+//let greet = greeting('Hello');
+//console.log(greet);
+//function calculateNumber(a:number,b:number,c:number):number {
+// let total:number= a+b-c;
+//return total
+//}
+//let result : number = calculateNumber (5,6,3);
+//console.log(result);
+//  Default parameter
+//function greetings(name: string , greeting: string ='hello'){
+//console.log(`${greeting}, ${name}`);
+//}
+//greetings('ali');
+//    Optional parameter
+//function optional(name?:string){
+//if(name) {
+// return `hello , ${name}`
+//} else {
+// return "Hello!";
+//}
+//}
+//console.log(optional());
+//console.log(optional("Ahmed"));
+//   Arrow function
+//let halfFryEgg = () => 1+ 1.5 + 3;
+//let response: number = halfFryEgg()
+//console.log(response);
+let halfFryEgg = (egg, oil, salt) => (egg + oil + salt);
+let response = halfFryEgg(1, 1.5, 2);
+console.log(response);
